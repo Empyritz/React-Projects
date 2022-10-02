c2fca40958acd1ab9973f5d86411e15080a673de
Fabian NP <fabian.nava.pereda@gmail.com>
2022-10-02 14:10:24 -0500

    Setup the project

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import SingleColor from './SingleColor';
+
+import Values from "values.js"
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
