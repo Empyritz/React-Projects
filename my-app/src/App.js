31e84d281a8832ae2f5774e4576ace7f5c0a4106
Fabian NP <fabian.nava.pereda@gmail.com>
2022-10-02 16:00:35 -0500

    getting a list of porcents of one hex color, so maped the list print on screen the values in rgb and hex, with the background of the current Color in the array, if you click the container this is copied to the navigate showing on screen a message of copied to clipoard and dissapear after 3 s with an useEffect each the alert state change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,45 @@
 import React, {useState} from 'react';
 import SingleColor from './SingleColor';
 
-import Values from "values.js"
+import Values from 'values.js'
 
 function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#FF8888').all(11))
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(11)
+      setList(colors)
+      setError(false)
+    } catch (error){
+      setError(true)
+    }
+  }
+
+  const colorList = list.map((color, index)=>{
+    return (
+      <SingleColor key={color.id} {...color} index={index} hexColor={color.hex}/>
+    )
+  })
+
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <section className='container'>
+        <h3>color generetor</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder='#f15025' className={error ? "error": null}/>
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+          {colorList}
+      </section>
+    </>
   );
 }
 
