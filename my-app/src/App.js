df0c24896667f021ddbf58a5a6fdfc97aae20f1d
Fabian NP <fabian.nava.pereda@gmail.com>
2022-10-01 22:57:00 -0500

    Making the state for all question in one state managing with props and validator functions bit it works and its awesome

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,43 @@ import data from "./data"
 import SingleQuestion from "./Question"
 
 function App() {
+  const arr = []
   const [questions, setQuestions] = useState(data)
+  const [showInfo, setShowInfo] = useState(arr)
+  const makeState = questions.forEach((question)=> { 
+    const currentInfo = {
+      id: question.id,
+      isShow: false,
+    }  
+    arr.push(currentInfo) 
+    
+   })
+   console.log(arr)
+
+
+   const toggleShow = (id) => {
+      setShowInfo((prevInfo)=>{
+        let state = []
+        for (let i = 0; i < showInfo.length; i++){
+        const currentQuestion = prevInfo[i]
+        if (currentQuestion.id === id ){
+          const updatedQuestion = {
+            ...currentQuestion,
+            isShow: !currentQuestion.isShow
+          }
+          state.push(updatedQuestion)
+          } else {
+            state.push(currentQuestion)
+          }
+        }
+        return state
+      })
+    }
+  
+
   const questionsList = questions.map((question)=>{
     return (
-      <SingleQuestion key={question.id} {...question}/>
+      <SingleQuestion key={question.id} {...question} toggleShow={toggleShow} stateShow={showInfo}/>
     )
   })
 
--- my-app/src/Question.js
+++ my-app/src/Question.js
@@ -2,18 +2,27 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 
-const Question = ({ title, info }) => {
+const Question = ({ title, info, toggleShow, stateShow, id }) => {
+    const validateShow = () => {
+        const current = stateShow.find((state)=>state.id === id)
+        if (current.isShow) {
+            return true
+        } else {
+            return false
+        }
+    }
+    let validator = validateShow()
 
     const [showInfo, setShowInfo] = useState(false)
     return (
         <article className='question'>
             <header>
                 <h4>{title}</h4>
-                <button className='btn' onClick={()=> setShowInfo(!showInfo)}>
-                    { showInfo? <AiOutlineMinus />: <AiOutlinePlus/> }
+                <button className='btn' onClick={()=>{toggleShow(id)}}>
+                    { validator? <AiOutlineMinus />: <AiOutlinePlus/> }
                 </button>
             </header>
-            { showInfo && <p>{info}</p> }
+            { validator && <p>{info}</p> }
             
         </article>
   );
