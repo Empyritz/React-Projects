624d28a020010f3c14797cb9444926f075834610
Fabian NP <fabian.nava.pereda@gmail.com>
2022-10-03 17:39:03 -0500

    Making calls to an api in differents way, with name for the home page and search cocktails that contains those strings and each the value change we make the call to the api to check the values, so if press the button details it obtain the id of thes object and print the id on the <Link> path to substract this with useParams and make other call to the api of the current id cocktail and display more info about it and displaying a custom page that manage and show this info

---
+++ my-app/src/components/SearchForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext()
+  const searchValue = React.useRef('')
+  // const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s'
+  // const urlID = 'www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007'
+  
+  React.useEffect(() => {
+    searchValue.current.focus()
+  },[])
+
+  const seacrhCocktail = ()=> {
+    setSearchTerm(searchValue.current.value)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+  }
+
+  return (
+    <section className='section search'>
+      <form action='' className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input type="text"id='name' ref={searchValue} onChange={seacrhCocktail}/>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ my-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>Exercitation voluptate id fugiat occaecat amet consequat. Incididunt enim elit ipsum exercitation laborum sunt elit sunt. Irure amet pariatur nulla amet ut laboris excepteur.</p>
+    </section>
+  )
+}
+
+export default About
